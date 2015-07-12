8218bf4982436a6b44116e23199227d210324bff
Tomek <tomasz.maslon@gmail.com>
2015-07-12 22:45:58 +0200

    Added Tabs on bottom with graphic,use TabActivity

---
+++ app/src/main/java/com/pum/tomasz/knowyourshare/ArrowsActivity.java
@@ -0,0 +1,17 @@
+package com.pum.tomasz.knowyourshare;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+/**
+ * @author Adil Soomro
+ *
+ */
+public class ArrowsActivity extends Activity {
+	
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.arrowspage);
+    }
+}
---
+++ app/src/main/java/com/pum/tomasz/knowyourshare/OptionsActivity.java
@@ -0,0 +1,18 @@
+package com.pum.tomasz.knowyourshare;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+/**
+ * @author Adil Soomro
+ *
+ */
+public class OptionsActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.optionspage);
+        
+        
+    }
+}
