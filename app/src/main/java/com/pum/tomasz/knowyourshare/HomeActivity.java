da7bf4858b573315a328be5e53d3947a3a90f095
U-AAITG\tmaslon <tmaslon@PLKRAL-020.aaitg.com>
2015-07-13 17:19:10 +0200

    Added new tabs drawable

--- app/src/main/java/com/pum/tomasz/knowyourshare/HomeActivity.java
+++ app/src/main/java/com/pum/tomasz/knowyourshare/HomeActivity.java
@@ -2,14 +2,30 @@ package com.pum.tomasz.knowyourshare;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 
 public class HomeActivity extends Activity {
+
+    private ImageButton homeImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        initializeTab();
     }
 
+    private void initializeTab(){
+        homeImageButton = (ImageButton)findViewById(R.id.home_button);
+        homeImageButton.setPressed(true);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initializeTab();
+    }
 }
